5302d7a63033543d42a041ed847ca6b8eb39c3ee
Shubham <shubhamshukla.shukla@gmail.com>
2017-04-09 21:55:15 +0530

    added button to show form in chooser activity

--- app/src/main/java/com/example/socket/smarthealthdetector/DoctorRegistartionActivity.java
+++ app/src/main/java/com/example/socket/smarthealthdetector/DoctorRegistartionActivity.java
@@ -3,11 +3,14 @@ package com.example.socket.smarthealthdetector;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class DoctorRegistartionActivity extends AppCompatActivity {
     EditText name, contact, address, speciality;
-
+    LinearLayout linearLayout;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +19,8 @@ public class DoctorRegistartionActivity extends AppCompatActivity {
         contact = (EditText) findViewById(R.id.contactno);
         address = (EditText) findViewById(R.id.address);
         speciality = (EditText) findViewById(R.id.speciality);
+        linearLayout= (LinearLayout) findViewById(R.id.doctor_registartion);
+        button= (Button) findViewById(R.id.register);
     }
 
     public void addDoc(View view) {
@@ -23,5 +28,9 @@ public class DoctorRegistartionActivity extends AppCompatActivity {
 
         db.addDoctor(new Doctor(name.getText().toString(), contact.getText().toString(), address.getText().toString(), speciality.getText().toString()));
 
+    }
+    public void showForm(View v) {
+        button.setVisibility(View.GONE);
+        linearLayout.setVisibility(View.VISIBLE);
     }
 }
